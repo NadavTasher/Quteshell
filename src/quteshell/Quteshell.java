47723c6ee6520e0b9fd5ebe9032f1e285b683e81
Nadav Tasger <tashernadav@gmail.com>
2019-10-14 11:03:30 +0300

    Fix

--- src/quteshell/Quteshell.java
+++ src/quteshell/Quteshell.java
@@ -80,8 +80,10 @@ public class Quteshell {
         ArrayList<Command> commands = new ArrayList<>();
         for (Command command : COMMANDS) {
             int elevation = command.getElevation();
-            if (elevation >= Elevation.ALL || elevation >= this.elevation) {
-                commands.add(command);
+            if(elevation!=Elevation.NONE){
+                if (elevation == Elevation.ALL || elevation >= this.elevation) {
+                    commands.add(command);
+                }
             }
         }
         return commands;
